dfe162448f5686fdce67a5f6467d1db8c7dcd8a7
Gumay <humaivier88@gmail.com>
2021-06-25 23:42:23 +0100

    Styling

--- src/components/Profile/Cover/Cover.tsx
+++ src/components/Profile/Cover/Cover.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import s from "./Cover.module.scss";
+import styles from "./Cover.module.scss";
 import { Avatar } from "./Avatar/Avatar";
 import { Info } from "./Info/Info";
 
 export const Cover = () => {
   return (
-    <div className={s.wrapper}>
+    <div className={styles.wrapper}>
       <Avatar />
       <Info />
     </div>
