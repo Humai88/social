f53d649ecf59767473121fe7014d68783a4aaf62
Gumay <humaivier88@gmail.com>
2021-09-17 12:36:02 +0100

    Add browserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import { store } from "./redux/reduxStore";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
